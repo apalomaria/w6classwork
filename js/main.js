5351c3fe916e8ea51adc6c95a63d3579cbc84b52
Alina Palomaria <alinaguzman.yearup@gmail.com>
2017-06-22 21:01:12 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(".A").click(function(){
+  $("img").show();
+});
+
+$( ".B").click(function(){
+  $("img").hide();
+});
+
+$(document).ready(function(){
+  $(".two h1").dblclick(function(){
+    $(".two h1").toggleClass("styleSwitch");
+  });
+});
+
+$(".add").click(function(){
+  $("ul").append("<li>Add</li>");
+});
+
+$(".four p").hover(function(){
+  $(this).toggleClass("hoverover");
+});
+
+$(".five h2").mouseleave(function(){
+  $(".five p").slideUp("slow");
+});
+
+$(".six p").hover(function(){
+  $(".six p").fadeOut("slow");
+});
+
+$(".over").click(function(){
+  $(".six p").fadeIn("slow");
+});
